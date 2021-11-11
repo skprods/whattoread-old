6e03cba46363a8d310ba29f5ef88d8ec8dae276e
Pavel Sklyar <ozpavel@yandex.ru>
2021-11-11 23:25:52 +0300

    feat: add datetime to dump's filename

--- deploy/percona/dump.sh
+++ deploy/percona/dump.sh
@@ -1,1 +1,5 @@
-mysqldump --user "$1" --password="$2" "$3" > /home/"$4".dump
+timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S" # current time
+}
+
+mysqldump --user "$1" --password="$2" "$3" > /home/"$3"_"$(timestamp)".dump
