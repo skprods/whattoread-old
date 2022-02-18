2e9df8a5c9e75d543dc0ef01fa1bf84c2585d05f
Pavel Sklyar <ozpavel@yandex.ru>
2022-02-18 11:38:38 +0300

    feat: run cron jobs with Laravel scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,36 +2,30 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ActualizeSystemCacheCommand;
+use App\Console\Commands\HealthCheckCommand;
+use App\Console\Commands\ReindexBooksCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
     protected $commands = [
-        //
+        ActualizeSystemCacheCommand::class,
+        HealthCheckCommand::class,
+        ReindexBooksCommand::class,
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        /** Актуализация кэша с информацией о системе */
+        $schedule->command(ActualizeSystemCacheCommand::class)->everyFiveMinutes();
+        /** Проверка работоспособности всех частей приложения */
+        $schedule->command(HealthCheckCommand::class)->everyFiveMinutes();
+        /** Полная переиндексация книг */
+        $schedule->command(ReindexBooksCommand::class)->dailyAt("01:00");
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
