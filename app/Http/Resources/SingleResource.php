66e5f43aa9feb9dd794306f80363fa143a26b203
Pavel Sklyar <ozpavel@yandex.ru>
2022-02-23 23:50:36 +0300

    feat: update documentation

--- app/Http/Resources/SingleResource.php
+++ app/Http/Resources/SingleResource.php
@@ -5,6 +5,12 @@ namespace App\Http\Resources;
 use App\Traits\CanPrepareData;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     description="Коллекция записей",
+ *     @OA\Property( property="success", type="boolean", example="true", description="Успешный ли ответ" ),
+ * )
+ */
 class SingleResource extends JsonResource
 {
     use CanPrepareData;
