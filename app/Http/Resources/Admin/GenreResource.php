3a67c846a3f931af8589dafefeb1c5abbe8582e3
Pavel Sklyar <ozpavel@yandex.ru>
2022-05-05 13:19:17 +0300

    fix: Исправлены отношения parents и childs в GenreResource

--- app/Http/Resources/Admin/GenreResource.php
+++ app/Http/Resources/Admin/GenreResource.php
@@ -63,8 +63,8 @@ class GenreResource extends SingleResource
         return [
             'id' => $this->resource->id,
             'name' => $this->resource->name,
-            'parent' => GenreResource::collection($this->whenLoaded('parent')),
-            'child' => GenreResource::collection($this->whenLoaded('child')),
+            'parent' => GenreResource::collection($this->whenLoaded('parents')),
+            'child' => GenreResource::collection($this->whenLoaded('childs')),
             'status' => $this->resource->status,
             'created_at' => $this->prepareDateTime($this->resource->created_at),
             'updated_at' => $this->prepareDateTime($this->resource->updated_at),
